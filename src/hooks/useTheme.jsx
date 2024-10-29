a2586af9f9d6550b40340524fbc7e01f67f4d3fd
abakou <abakoubusiness@gmail.com>
2024-10-30 00:14:29 +0100

    wip

--- src/hooks/useTheme.jsx
+++ src/hooks/useTheme.jsx
@@ -1,45 +1,43 @@
 import { useEffect, useState } from "react"
+import { getDefaultTheme, htmlNodeClassList } from "../lib/utils";
 
 export default function useTheme() {
 
-    useEffect(() => {
-        let mode = localStorage.getItem('mode')
-        if (mode == 'dark' || mode == null) {
-            dark()
-        }
-    }, []);
+    useEffect(() => { (getDefaultTheme() == 'dark') && dark() }, []);
 
     const [_, __] = useState(true)
 
-    function refresh() {
+    function refresh(persist = false) {
         __(!_)
-        localStorage.setItem('mode', isDark() ? 'dark' : 'light')
+        if (persist) {
+            localStorage.setItem('theme', isDark() ? 'dark' : 'light')
+        }
     }
 
-    function toogle(params) {
+    function toogle(persist = false) {
         window.document.querySelector('html').classList.toggle('dark')
-        refresh()
+        refresh(persist)
     }
 
-    function dark() {
-        if (!window.document.querySelector('html').classList.contains('dark')) {
-            window.document.querySelector('html').classList.add('dark')
-            refresh()
+    function dark(persist = false) {
+        if (!htmlNodeClassList().contains('dark')) {
+            htmlNodeClassList().add('dark')
+            refresh(persist)
         }
     }
 
-    function light() {
-        if (window.document.querySelector('html').classList.contains('dark')) {
-            window.document.querySelector('html').classList.remove('dark')
-            refresh()
+    function light(persist = false) {
+        if (htmlNodeClassList().contains('dark')) {
+            htmlNodeClassList().remove('dark');
+            refresh(persist)
         }
     }
 
-    function isDark(params) {
-        return document.querySelector('html').classList.contains('dark')
+    function isDark() {
+        return htmlNodeClassList().contains('dark')
     }
 
-    function isLight(params) {
+    function isLight() {
         return !isDark()
     }
 
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,6 @@
-import {clsx } from "clsx";
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
- 
+
 /**
  * 
  * @param  {...string} inputs 
@@ -8,4 +8,20 @@ import { twMerge } from "tailwind-merge";
  */
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+
+
+export function getDefaultTheme() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    return savedTheme;
+  } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  } else {
+    return 'light';
+  }
+}
+
+export function htmlNodeClassList() {
+  return window.document.querySelector('html').classList
 }
